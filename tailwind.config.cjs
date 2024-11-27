567cb6916d825430e52e467511dc4d77bcb260bc
Eirlys Vo <voquyphiqn2004@gmail.com>
2024-11-26 21:57:44 -0500

    done styles and items. need to do location and space between elements

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,12 +11,28 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
-				mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono]
+				sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
+				mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+				exo2: ['Exo 2', ...defaultTheme.fontFamily.sans],
+				poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
+				quicksand: ['Quicksand', ...defaultTheme.fontFamily.sans],
+				rajdhani: ['Rajdhani', ...defaultTheme.fontFamily.sans],
+				robotoMono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+				openSans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+				nunito: ['Nunito', ...defaultTheme.fontFamily.sans],
+				pacifico: ['Pacifico', ...defaultTheme.fontFamily.sans],
+				dmSerifDisplay: ['DM Serif Display', ...defaultTheme.fontFamily.serif],
+				teko: ['Teko', ...defaultTheme.fontFamily.sans],
 			},
 			gridTemplateColumns: {
 				'22': 'repeat(22, minmax(0, 1fr))',
 			},
+			colors: {
+				customDarkBlue: "#20385e",
+				customPink: "#e48279",
+				customGreen: "#1BAFAF",
+				customLightGreen: "#9DD9BD",
+			}
 		},
 	},
 	plugins: [],
