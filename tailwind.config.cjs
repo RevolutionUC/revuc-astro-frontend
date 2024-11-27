f315bb0d63fc7892a799b6595c9cd3abb45bd141
Eirlys Vo <voquyphiqn2004@gmail.com>
2024-11-27 17:10:49 -0500

    updated colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,7 +12,7 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				sans: ["Noto Sans", ...defaultTheme.fontFamily.sans],
-				mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+				mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
 				exo2: ['Exo 2', ...defaultTheme.fontFamily.sans],
 				poppins: ['Poppins', ...defaultTheme.fontFamily.sans],
 				quicksand: ['Quicksand', ...defaultTheme.fontFamily.sans],
@@ -31,7 +31,7 @@ module.exports = {
 				'theme-teal': '#1BAFAF',
 				'theme-dark-blue': '#20385E',
 				'theme-coral': '#E48279',
-				'theme-pastel-green': '9DD9BD',
+				'theme-pastel-green': '#9DD9BD',
 				'theme-cream': '#FDF9F6',
 				'theme-light-teal': '#CAF1F1',
 				'theme-light-coral': '#F8DBD8',
